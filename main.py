852bea3f051c10af605aa52b995001f23d1053ee
Votoryncev Dmitry <w.dmkvo@gmail.com>
2023-05-14 19:05:15 +0300

    refactored main.py

--- main.py
+++ main.py
@@ -22,45 +22,45 @@ def read_config(config_file: str) -> list[Site]:
     for site in config.sections():
         lst = config[site]["network_ranges"].split("\n")
         ranges = set(tuple(x.replace("\n", "").split("-")) for x in lst)
-        sites.append(Site(
-            config[site].name,
-            int(config[site]["chat_id"]),
-            config[site]["client_id"],
-            ranges
-        ))
+        sites.append(
+            Site(
+                config[site].name,
+                int(config[site]["chat_id"]),
+                config[site]["client_id"],
+                ranges
+            )
+        )
     return sites
+
+
+async def analyze_site(site: Site):
+    logging.info(f"Start scanning: {site.name}")
+    scanned = await site.scan_all()
+    logging.info(f"Total scanned: {len(scanned)}")
+    foreman = site.scan_foreman()
+    logging.info(f"Total in Foreman: {len(foreman)}")
+    missed_miners = {
+        ip: workername
+        for ip, workername in scanned.items()
+        if ip not in foreman and workername not in foreman.values()
+    }
+    logging.info(f"Missed: {len(missed_miners)}")
+    if missed_miners:
+        miners_stats = {
+            "scanned": len(scanned),
+            "foreman": len(foreman),
+            "missed": missed_miners
+        }
+        await send_report(site.chat_id, miners_stats)
 
 
 async def main():
     configure_logging()
     logging.info("Script started")
-    # Перебираем все конфигурации из файла CONFIG_FILE
     for site in read_config(CONFIG_FILE):
-        logging.info(f"Start scanning: {site.name}")
-        # Получение майнеров, отсканированных в заданных диапазонах
-        scanned = await site.scan_all()
-        logging.info(f"Total scanned: {len(scanned)}")
-
-        # Получение майнеров, зарегистрированных в Foreman API
-        foreman = site.scan_foreman()
-        logging.info(f"Total in Foreman: {len(foreman)}")
-
-        # Поиск отсутствующих майнеров в Foreman
-        miners = {
-            "scanned": len(scanned),
-            "foreman": len(foreman),
-            "missed": {}
-        }
-        for ip, workername in scanned.items():
-            if ip not in foreman and workername not in foreman.values():
-                miners.get("missed")[ip] = workername
-        logging.info(f"Missed: {len(miners.get('missed'))}")
-
-        if len(miners.get("missed")) > 0:
-            # Отправка отчета в телеграм
-            await send_report(site.chat_id, miners)
-
+        await analyze_site(site)
     logging.info("Script finished")
+
 
 if __name__ == "__main__":
     asyncio.run(main())
